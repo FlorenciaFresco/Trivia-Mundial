05ffcdf6de8cea8372bb19b24606d342cca0d1c1
Florencia Fresco <110852312+FlorenciaFresco@users.noreply.github.com>
2022-12-27 18:22:22 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const ganadores = ["Uruguay 1930", "Italia 1934", "Italia 1938", "Uruguay 1950", "Alemania 1954", "Brasil 1958", "Brasil 1962", "Inglaterra 1966", "Brasil 1970", "Alemania 1974", "Argentina 1978", "Italia 1982", "Argentina 1986", "Alemania 1990", "Brasil 1994", "Francia 1998", "Brasil 2002", "Italia 2006", "España 2010", "Alemania 2014", "Francia 2018", "Argentina 2022"]
+
+console.log(ganadores);
+console.log(ganadores[10]);
+console.log(ganadores[12]);
+console.log(ganadores[21]);
+
 const quizData = [
     {
         question: "1. ¿Cuándo tuvo lugar la primera Copa Mundial de Fútbol?",
