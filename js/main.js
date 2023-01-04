3aed3badb6906c4745f85ab9d20d9b178520ab6b
Florencia Fresco <110852312+FlorenciaFresco@users.noreply.github.com>
2023-01-04 17:19:02 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-function saludar(nombre) {
-    console.log("Hola " + nombre + " vamos a ver cuando mundiales gano Argentina");
+function saludar() {
+    const nombre = prompt('Ingrese su nombre, porfavor')
+    if (nombre.trim() === "") {
+        alert('No ingresaste ningun dato, porfavor ingrese su nombre')
+        saludar()
+    } else {
+        alert(`Te doy la bienvenida ${nombre}`)
+    }
+    console.log("Hola " + nombre + " vamos a ver cuanto sabes del mundial!");
 }
 saludar("Profesor");
 
@@ -9,38 +16,104 @@ if (edad >= 18) {
 } else {
     alert("Sos menor de edad. No podés ingresar")
 }
-const ganadores = ["Uruguay 1930", "Italia 1934", "Italia 1938", "Uruguay 1950", "Alemania 1954", "Brasil 1958", "Brasil 1962", "Inglaterra 1966", "Brasil 1970", "Alemania 1974", "Argentina 1978", "Italia 1982", "Argentina 1986", "Alemania 1990", "Brasil 1994", "Francia 1998", "Brasil 2002", "Italia 2006", "España 2010", "Alemania 2014", "Francia 2018", "Argentina 2022"]
+const quizData = [
+    {
+        question: "1. ¿Cuándo tuvo lugar la primera Copa Mundial de Fútbol?",
+        a: "1920",
+        b: "1930",
+        c: "1940",
+        d: "1950",
+        correct: "b",
+    },
+    {
+        question: "2. ¿Quién es el máximo goleador de la historia de la Copa Mundial de Fútbol?",
+        a: "Miroslav Klose",
+        b: "Ronaldo Nazario",
+        c: "Gerd Müller",
+        d: "Pelé",
+        correct: "a",
+    },
+    {
+        question: "3. ¿Qué animal fue el escogido para ilustrar la mascota del Mundial de Rusia?",
+        a: "Un perro",
+        b: "Un gato",
+        c: "Un lobo",
+        d: "Un loro",
+        correct: "c",
+    },
+    {
+        question: "4. Hablando de mascotas, ¿cuál de estas mascotas no pertenece a la historia del Mundial?",
+        a: "Fuleco",
+        b: "Footix",
+        c: "Naranjito",
+        d: "TopTip",
+        correct: "d",
+    },
+    {
+        question: "5. ¿Cuántos mundiales de fútbol ha ganado Portugal?",
+        a: "0",
+        b: "1",
+        c: "2",
+        d: "3",
+        correct: "a",
+    },
+    {
+        question: "6. ¿Qué nombre recibe el balón oficial?",
+        a: "Mannschaft",
+        b: "Telstar",
+        c: "Bildo",
+        d: "Ballstar",
+        correct: "b",
+    },
+    {
+        question: "7. ¿Qué equipo ganó el mundial en 1986?",
+        a: "Argentina",
+        b: "Italia",
+        c: "Francia",
+        d: "Alemania",
+        correct: "a"
+    },
+];
 
-console.log(ganadores);
-console.log(ganadores[10]);
-console.log(ganadores[12]);
-console.log(ganadores[21]);
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
 
-ganadores.push("Argentina 2026?");
-console.log(ganadores);
 
-console.log(ganadores.indexOf("Argentina 2022"));
+let currentQuiz = 0
+let score = 0
 
-var questions = [{
-    question: ' Cuantas veces Argentina salio campeon?',
-    answer: '3',
-    affirm: 'Yay! Correcto!',
-    rebuttal: 'Error!.'
-},
-{
-    question: 'Cuantas veces salio Francia Campeon?',
-    answer: '2',
-    affirm: 'Good job!',
-    rebuttal: 'Not right.'
-}];
+loadQuiz()
 
-for (var i = 0, l = questions.length; i < l; i++) {
-    answer = prompt(questions[i].question);
+function loadQuiz() {
 
-    if (answer !== questions[i].answer) {
-        alert(questions[i].rebuttal);
-        i--;
-    } else {
-        alert(questions[i].affirm);
-    }
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
 }
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if (answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
