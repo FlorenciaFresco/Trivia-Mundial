fff48d2cea7d6dcbf396f18d66bc85852d6126ce
Florencia Fresco <110852312+FlorenciaFresco@users.noreply.github.com>
2023-02-02 16:42:30 -0300

    Update noticias.js

--- js/noticias.js
+++ js/noticias.js
@@ -13,3 +13,29 @@ function mostrarGanadores(ganadores) {
         paisesGanadores.append(li);
     });
 }
+
+
+class Campeones {
+    constructor(pais, trofeos) {
+        this.pais = pais;
+        this.trofeos = trofeos;
+    }
+}
+
+const brasil = new Campeones("Brasil", "5 trofeos");
+const alemania = new Campeones("Alemania", "4 trofeos");
+const italia = new Campeones("Italia", "4 trofeos");
+const argentina = new Campeones("Argentina", "3 trofeos");
+const francia = new Campeones("Francia", "2 trofeos");
+const uruguay = new Campeones("Uruguay", "2 trofeos");
+const españa = new Campeones("España", "1 trofeos");
+const inglaterra = new Campeones("Inglaterra", "1 trofeos");
+
+console.log(brasil.trofeos);
+console.log(alemania.trofeos);
+console.log(italia.trofeos);
+console.log(argentina.trofeos);
+console.log(francia.trofeos);
+console.log(uruguay.trofeos);
+console.log(españa.trofeos);
+console.log(inglaterra.trofeos);
